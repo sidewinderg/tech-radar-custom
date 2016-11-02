4977c46473ce8d794c5e89cc411f18cc0bb3431c
George Green <sidewinderg@gmail.com>
2016-11-02 13:00:27 -0500

    Creating data structure #2
    Creating the initial data structure for the radar app

--- data/radarData.js
+++ data/radarData.js
@@ -1,4 +1,165 @@
 //Data file for formatted json data
-var radar_data = [{
-
-}];
+var radar_data = [
+  {
+    "_id": "581386317793223276ae4cad",
+    "quadrant": 1,
+    "ring": 0,
+    "confidence": 0,
+    "team": 0,
+    "hasChangedOrNew": false,
+    "isActive": true,
+    "guid": "2252177d-6868-417f-8b73-563032edb666",
+    "index": 0,
+    "info": {
+      "shortName": "EMOLTRA",
+      "initials": "D",
+      "fullName": "URBANSHEE Hewitt",
+      "details": "magna dolor Lorem nostrud quis adipisicing quis cupidatat laboris sunt mollit ad ex minim ex incididunt commodo dolor eu minim laborum in nostrud magna ex deserunt irure in sit sint esse ipsum dolor fugiat deserunt consequat ut est exercitation amet",
+      "logo": "http://placehold.it/32x32"
+    }
+  },
+  {
+    "_id": "581386317fea99732b25d8f5",
+    "quadrant": 2,
+    "ring": 0,
+    "confidence": 3,
+    "team": 1,
+    "hasChangedOrNew": false,
+    "isActive": false,
+    "guid": "0a3ad76d-2eb5-4b1b-8a1a-761fadb55a65",
+    "index": 1,
+    "info": {
+      "shortName": "EGYPTO",
+      "initials": "E",
+      "fullName": "BOILCAT Bates",
+      "details": "amet veniam ullamco consequat dolor duis dolore in quis irure deserunt quis adipisicing officia adipisicing nulla veniam enim ea eu excepteur pariatur id in pariatur occaecat quis exercitation duis quis proident dolore mollit deserunt occaecat adipisicing tempor eiusmod exercitation sint",
+      "logo": "http://placehold.it/32x32"
+    }
+  },
+  {
+    "_id": "5813863158f5fa888203ea3c",
+    "quadrant": 0,
+    "ring": 3,
+    "confidence": 3,
+    "team": 2,
+    "hasChangedOrNew": true,
+    "isActive": true,
+    "guid": "09d0156e-042a-4908-8318-4c5c2b0c1c59",
+    "index": 2,
+    "info": {
+      "shortName": "PLASMOSIS",
+      "initials": "T",
+      "fullName": "FITCORE Dale",
+      "details": "pariatur dolore laboris nostrud esse sint deserunt laborum Lorem qui irure et laborum culpa velit nisi amet elit cupidatat et sint est culpa ullamco aliqua nulla sit minim incididunt officia nisi sunt culpa ullamco anim dolor Lorem exercitation consequat nulla",
+      "logo": "http://placehold.it/32x32"
+    }
+  },
+  {
+    "_id": "581386319fd5cb0c1a93077c",
+    "quadrant": 4,
+    "ring": 1,
+    "confidence": 0,
+    "team": 1,
+    "hasChangedOrNew": true,
+    "isActive": false,
+    "guid": "b1d0accc-bd8e-4b12-a6df-4fcfcd92b018",
+    "index": 3,
+    "info": {
+      "shortName": "COMTOURS",
+      "initials": "O",
+      "fullName": "IPLAX Fitzgerald",
+      "details": "pariatur nisi aliquip proident voluptate elit in sit esse culpa consectetur veniam mollit do adipisicing proident exercitation et nostrud ea cillum irure eu aliquip aute amet mollit esse aliqua duis sunt et tempor tempor laborum deserunt ut sint tempor velit",
+      "logo": "http://placehold.it/32x32"
+    }
+  },
+  {
+    "_id": "5813863182f2b457230f8f20",
+    "quadrant": 1,
+    "ring": 0,
+    "confidence": 1,
+    "team": 2,
+    "hasChangedOrNew": true,
+    "isActive": true,
+    "guid": "1b0ea9e9-a180-4ec0-a5ab-4617cc815c63",
+    "index": 4,
+    "info": {
+      "shortName": "ZOLAREX",
+      "initials": "E",
+      "fullName": "LIQUICOM Reyes",
+      "details": "et exercitation duis officia consequat veniam ad deserunt velit minim mollit esse nisi velit do Lorem sint quis cupidatat voluptate consequat esse cillum veniam ad et tempor excepteur culpa in Lorem do mollit qui deserunt veniam proident quis in enim",
+      "logo": "http://placehold.it/32x32"
+    }
+  },
+  {
+    "_id": "581386319e29283f94c63d14",
+    "quadrant": 2,
+    "ring": 0,
+    "confidence": 3,
+    "team": 1,
+    "hasChangedOrNew": true,
+    "isActive": false,
+    "guid": "83740bd8-6bb6-4842-9837-b5bb7737aaf5",
+    "index": 5,
+    "info": {
+      "shortName": "QUANTALIA",
+      "initials": "E",
+      "fullName": "VETRON Solis",
+      "details": "ad exercitation laborum proident nisi officia laboris magna irure id ex veniam aliquip quis ut aliqua occaecat nisi non do irure consectetur qui cupidatat enim quis duis fugiat incididunt anim officia do duis ullamco non esse dolor Lorem do non",
+      "logo": "http://placehold.it/32x32"
+    }
+  },
+  {
+    "_id": "581386313222a5a1befccbfe",
+    "quadrant": 3,
+    "ring": 0,
+    "confidence": 2,
+    "team": 0,
+    "hasChangedOrNew": false,
+    "isActive": true,
+    "guid": "9da4e724-321f-4a90-894f-711a1d4d8801",
+    "index": 6,
+    "info": {
+      "shortName": "PHOLIO",
+      "initials": "M",
+      "fullName": "NIPAZ Harris",
+      "details": "ut laborum velit aute incididunt qui qui sunt duis sint est proident incididunt eu sit deserunt proident duis fugiat excepteur irure ipsum ex fugiat commodo veniam occaecat culpa eu reprehenderit ut sit in ex incididunt laboris velit labore et consequat",
+      "logo": "http://placehold.it/32x32"
+    }
+  },
+  {
+    "_id": "58138631b466c74484986770",
+    "quadrant": 1,
+    "ring": 1,
+    "confidence": 1,
+    "team": 1,
+    "hasChangedOrNew": false,
+    "isActive": false,
+    "guid": "5d91290e-e5c6-4af0-b5a4-791fb042a8e7",
+    "index": 7,
+    "info": {
+      "shortName": "REALMO",
+      "initials": "Q",
+      "fullName": "ACUMENTOR Kennedy",
+      "details": "pariatur minim fugiat labore tempor nulla incididunt exercitation mollit ipsum excepteur non enim qui esse quis enim aliquip voluptate laborum laboris mollit voluptate non consequat qui commodo est aliqua minim cupidatat ullamco eu ullamco occaecat culpa excepteur minim nulla nulla",
+      "logo": "http://placehold.it/32x32"
+    }
+  },
+  {
+    "_id": "5813863179cb1894cd3f9191",
+    "quadrant": 4,
+    "ring": 2,
+    "confidence": 4,
+    "team": 0,
+    "hasChangedOrNew": false,
+    "isActive": false,
+    "guid": "4cae0278-41ad-4a41-9c0d-d14667ad7a52",
+    "index": 8,
+    "info": {
+      "shortName": "VERBUS",
+      "initials": "D",
+      "fullName": "UBERLUX Bender",
+      "details": "in eiusmod Lorem exercitation Lorem proident nostrud commodo excepteur cupidatat ex in eiusmod aliqua sit ex cillum sunt et excepteur excepteur amet do amet sint commodo non id dolore non reprehenderit velit fugiat et do deserunt nisi commodo nulla amet",
+      "logo": "http://placehold.it/32x32"
+    }
+  }
+];
